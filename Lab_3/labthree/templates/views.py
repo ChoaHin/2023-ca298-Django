f54679e24d1de6de2cf8b8fada06422a39c1f634
choac2 <chee.choa2@mail.dcu.ie>
2023-02-01 09:38:42 +0000

    builded database model

--- Lab_3/labthree/templates/views.py
+++ Lab_3/labthree/templates/views.py
@@ -4,3 +4,23 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+def view_all_books(request):
+    all_books = Book.objects.all()
+    return render(request, 'all_books.html', {'books':all_books})
+
+def view_single_book(request, bookid):
+    single_book = get_object_or_404(Book, id=bookid)
+    return render(request, 'single_book.html', {'book':single_book})
+
+def view_specific_year(request, year):
+    specific_year = Book.objects.filter(year = year)
+    return render(request, 'all_books.html', {'books':specific_year})
+
+def view_specific_category(request, category):
+    specific_category = Book.objects.filter(category__iexact=category)
+    return render(request, 'all_books.html', {'books':specific_category})
+
+def view_category_year(request, category, year):
+    specific_category_year = Book.objects.filter(category__iexact=category, year = year)
+    return render(request,'all_books.html', {'books':specific_category_year}  )
