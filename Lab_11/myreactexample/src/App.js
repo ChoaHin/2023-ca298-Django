e3ea9fc399a7d202458d0e2fc565a6fe33348e79
Chee Hin Choa <chee.choa2@mail.dcu.ie>
2023-03-31 16:28:07 +0100

    added header

--- Lab_11/myreactexample/src/App.js
+++ Lab_11/myreactexample/src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+function HeadingComponent(){
+	return (
+		<h1>This is my first component</h1>
+	)
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <HeadingComponent />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
